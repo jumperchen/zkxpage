b8eceb0003dbc952bf0950f1359cf976f587aee8
atticcat@gmail.com <atticcat@gmail.com@6fc6b4a6-9713-a132-4eaa-7828938304fc>
2011-07-25 06:30:55 +0000

    add GPL v3 license declaration

--- trunk/org.zkoss.xpage.core/src/org/zkoss/xpage/core/XspLibrary.java
+++ trunk/org.zkoss.xpage.core/src/org/zkoss/xpage/core/XspLibrary.java
@@ -1,18 +1,18 @@
-/* 
-{{IS_NOTE
-	Purpose:
-		
-	Description:
-		
-	History:
-		July 13, 2011 , Created by dennischen
-}}IS_NOTE
-
-Copyright (C) 2010 Potix Corporation. All Rights Reserved.
-
-{{IS_RIGHT
-}}IS_RIGHT
-*/
+/*
+ * Copyright 2011 Potix Corporation. All Rights Reserved.
+ * 
+ * Licensed under the GNU GENERAL PUBLIC LICENSE Version 3 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
 package org.zkoss.xpage.core;
 
 import com.ibm.xsp.library.AbstractXspLibrary;
@@ -45,7 +45,7 @@ public class XspLibrary extends AbstractXspLibrary {
 
 	@Override
 	public String getTagVersion() {
-		return "0.8.0";
+		return "0.9.0";
 	}
 
 	
--- trunk/org.zkoss.xpage.core/src/org/zkoss/xpage/core/web/ExampleServlet.java
+++ trunk/org.zkoss.xpage.core/src/org/zkoss/xpage/core/web/ExampleServlet.java
@@ -1,18 +1,18 @@
-/* 
-{{IS_NOTE
-	Purpose:
-		
-	Description:
-		
-	History:
-		July 13, 2011 , Created by dennischen
-}}IS_NOTE
-
-Copyright (C) 2010 Potix Corporation. All Rights Reserved.
-
-{{IS_RIGHT
-}}IS_RIGHT
-*/
+/*
+ * Copyright 2011 Potix Corporation. All Rights Reserved.
+ * 
+ * Licensed under the GNU GENERAL PUBLIC LICENSE Version 3 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
 package org.zkoss.xpage.core.web;
 
 import java.io.IOException;
